7046d9564255ed2b0c89f5e09c6ddc4f46e7d6b8
Emmanuel Kwabena Asamoah Oduro-Opong <150050720+dru-pong@users.noreply.github.com>
2026-02-27 14:07:04 +0000

    Fix db error

--- config/settings.py
+++ config/settings.py
@@ -76,10 +76,7 @@ DATABASES = {
         default=os.environ.get('DATABASE_URL'),
         conn_max_age=600
     ),
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+
 }
 
 
